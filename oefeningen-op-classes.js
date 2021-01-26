371610a8d38e5e626743612add4c04ad6584d133
Marvi Hendriks <marvihendriks@gmail.com>
2021-01-26 20:44:43 +0100

    oefeningen geplaatst over classes

---
+++ oefeningen-op-classes.js
@@ -0,0 +1,62 @@
+/*
+_________________________OEFENING 1_____________________________
+
+Create a class that takes the following four arguments for a particular football player:
+
+name
+age
+height
+weight
+Also, create three functions for the class that returns the following strings:
+
+getAge() returns "name is age age"
+getHeight() returns "name is heightcm"
+getWeight() returns "name weighs weightkg"
+
+
+___________________________OEFENING 2______________________________
+
+Create functions for the Calculator class that can do the following:
+
+Add two numbers.
+Subtract two numbers.
+Multiply two numbers.
+Divide two numbers.
+
+
+Examples
+var calculator = new Calculator()
+
+calculator.add(10, 5) ➞ 15
+
+calculator.subtract(10, 5) ➞ 5
+
+calculator.multiply(10, 5) ➞ 50
+
+calculator.divide(10, 5) ➞ 2
+
+______________________________OEFENING 3___________________________
+
+Given a class for a BasicPlan, write the classes for StandardPlan and PremiumPlan which have class properties of the following:
+
+BasicPlan	StandardPlan	PremiumPlan
+✓	           ✓            	✓	canStream
+✓	           ✓	            ✓	canDownload
+✓	          ✓             	✓	hasSD
+✓	          ✓	                    hasHD
+✓	                                hasUHD
+1	          2	                 4	numOfDevices
+$8.99	       $12.99	    $15.99	price
+
+
+Examples
+BasicPlan.hasSD ➞ true 
+
+PremiumPlan.hasSD ➞ true
+
+BasicPlan.hasUHD ➞ false
+
+BasicPlan.price ➞ '$8.99'
+
+PremiumPlan.numOfDevices ➞ 4
+*/
